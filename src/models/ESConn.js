d6a39d2026e3c9c6be199290411c7dd89020baa1
Rafael Martins <rsmartins78@hotmail.com>
2019-06-28 11:35:01 -0300

    getTime as lib, improved payload of addDevice

--- src/models/ESConn.js
+++ src/models/ESConn.js
@@ -1,5 +1,6 @@
 const { Client } = require("elasticsearch");
 const { encryptData } = require("../../lib/encrypt");
+const { getTime } = require("../../lib/getTime")
 
 const client = new Client({
   host: process.env.ELASTIC_HOST || "http://localhost:9200/",
@@ -11,35 +12,6 @@ const client = new Client({
 client.cluster.health({}, function(err, health) {
   console.log("\n-- ElasticSearch Health --\n", health, "\n -- End --\n");
 });
-
-async function getTime() {
-  var today = new Date();
-  var DD = today.getDate();
-  var MM = today.getMonth() + 1;
-  var YYYY = today.getFullYear();
-
-  var hh = today.getHours();
-  var mm = today.getMinutes();
-  var ss = today.getSeconds();
-
-  if (DD < 10) {
-    var DD = "0" + DD;
-  }
-  if (MM < 10) {
-    var MM = "0" + MM;
-  }
-  if (mm < 10) {
-    var mm = "0" + mm;
-  }
-  if (hh < 10) {
-    var hh = "0" + hh;
-  }
-  if (ss < 10) {
-    var ss = "0" + ss;
-  }
-  return (createdAt =
-    YYYY + "/" + MM + "/" + DD + " " + hh + ":" + mm + ":" + ss);
-}
 
 async function checkFirstAdmin() {
   const passwd = await encryptData(process.env.ADMIN_PASS || "admin");
