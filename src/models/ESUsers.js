138705f7d69929cf1d8067b0428a7b1649dbf667
Rafael Martins <rsmartins78@hotmail.com>
2019-07-01 18:20:13 -0300

    correção no lastloginat

--- src/models/ESUsers.js
+++ src/models/ESUsers.js
@@ -1,63 +1,67 @@
-const client = require('./ESConn');
-const { encryptData } = require('../../lib/encrypt');
+const client = require("./ESConn");
+const { encryptData } = require("../../lib/encrypt");
+const { getTime } = require("../../lib/getTime");
 
 module.exports = {
   async validateLogin(user) {
     const result = await client.search({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       body: {
         query: {
           bool: {
             must: [
               {
                 term: {
-                  'username.keyword': user,
-                },
-              },
-            ],
-          },
-        },
-      },
+                  "username.keyword": user
+                }
+              }
+            ]
+          }
+        }
+      }
     });
     return result;
   },
   async createUser(user, password, group) {
+    const time = await getTime();
+
     const result = await client.index({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       body: {
         username: user,
         password: encryptData(password),
         group,
-        createdAt: Date(),
-        lastLoginAt: Date(),
-      },
+        createdAt: time,
+        lastLoginAt: time
+      }
     });
     return result;
   },
   async updateLoginAt(user, callback) {
     const search = await client.search({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       body: {
         query: {
           bool: {
             must: [
               {
                 term: {
-                  'username.keyword': user,
-                },
-              },
-            ],
-          },
-        },
-      },
+                  "username.keyword": user
+                }
+              }
+            ]
+          }
+        }
+      }
     });
     const userId = search.hits.hits[0]._id;
+    const time = await getTime();
     const resultUpdate = await client.update({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       id: userId,
       body: {
         doc: {
@@ -65,34 +69,34 @@ module.exports = {
           password: search.hits.hits[0]._source.password,
           group: search.hits.hits[0]._source.group,
           createdAt: search.hits.hits[0]._source.createdAt,
-          lastLoginAt: Date(),
-        },
-      },
+          lastLoginAt: time
+        }
+      }
     });
     return resultUpdate;
   },
   async updateUserPassword(user, password) {
     const search = await client.search({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       body: {
         query: {
           bool: {
             must: [
               {
                 term: {
-                  'username.keyword': user,
-                },
-              },
-            ],
-          },
-        },
-      },
+                  "username.keyword": user
+                }
+              }
+            ]
+          }
+        }
+      }
     });
     const userId = search.hits.hits[0]._id;
     const resultUpdate = await client.update({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       id: userId,
       body: {
         doc: {
@@ -100,44 +104,44 @@ module.exports = {
           password,
           group: search.hits.hits[0]._source.group,
           createdAt: search.hits.hits[0]._source.createdAt,
-          lastLoginAt: search.hits.hits[0]._source.lastLoginAt,
-        },
-      },
+          lastLoginAt: search.hits.hits[0]._source.lastLoginAt
+        }
+      }
     });
     return resultUpdate;
   },
   async getUsers() {
     const result = await client.search({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       body: {
         query: {
-          match_all: {},
-        },
-      },
+          match_all: {}
+        }
+      }
     });
     return result;
   },
   async getUserById(id) {
     const result = await client.search({
-      index: 'screenly-users',
-      type: 'users',
+      index: "screenly-users",
+      type: "users",
       body: {
         query: {
           match: {
-            _id: id,
-          },
-        },
-      },
+            _id: id
+          }
+        }
+      }
     });
     return result;
   },
   async deleteUserById(id) {
     const result = await client.delete({
-      index: 'screenly-users',
+      index: "screenly-users",
       id,
-      type: 'users',
+      type: "users"
     });
     return result;
-  },
+  }
 };
