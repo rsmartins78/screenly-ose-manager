2d71c511a0f444769485b553c00e6ef17bb9a0be
Rafael Martins <rsmartins78@hotmail.com>
2019-07-24 17:48:16 -0300

    correction of cabaçada

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -29,7 +29,7 @@ module.exports = {
           res.status(200).send({
             success: true,
             message: "user created with success",
-            username: user,
+            username: data.user,
             id: resultUser._id
           });
         } else {
